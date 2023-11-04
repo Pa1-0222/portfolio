e0576e8934b9107a55edb3d6c5352325902fe947
Pa1-0222 <143471735+Pa1-0222@users.noreply.github.com>
2023-11-04 21:44:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+let typed = new Typed(".text", {
+    strings: ["Frontend Developer", "web Developer", "Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
